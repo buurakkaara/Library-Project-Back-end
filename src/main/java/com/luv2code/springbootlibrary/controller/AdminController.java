b4839d542d3805de3dba5dd9867256d97d196984
Burak KARA <brkkr36x@gmail.com>
2023-04-16 15:20:48 +0200

    New updates

---
+++ src/main/java/com/luv2code/springbootlibrary/controller/AdminController.java
@@ -0,0 +1,89 @@
+package com.luv2code.springbootlibrary.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springbootlibrary.requestmodels.AddBookRequest;
+import com.luv2code.springbootlibrary.service.AdminService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+
+@CrossOrigin("https://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+	private AdminService adminService;
+
+	public AdminController(AdminService adminService) {
+		this.adminService = adminService;
+	}
+	
+	@PostMapping("/secure/add/book")
+	public void postBook(@RequestHeader(value = "Authorization") String token,
+							@RequestBody AddBookRequest addBookRequest) throws Exception{
+		String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+		if (admin == null || !admin.equals("admin")) {
+			throw new Exception("Admin page only");
+		}
+		
+		adminService.postBook(addBookRequest);
+	}
+	
+	@PutMapping("/secure/increase/book/quantity")
+	public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+									@RequestParam Long bookId) throws Exception {
+		String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+		if (admin == null || !admin.equals("admin")) {
+			throw new Exception("Admin page only");
+		}
+		
+		adminService.increaseBookQuantity(bookId);
+		
+	}	
+
+	@PutMapping("/secure/decrease/book/quantity")
+	public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token,
+									@RequestParam Long bookId) throws Exception {
+		String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+		if (admin == null || !admin.equals("admin")) {
+			throw new Exception("Admin page only");
+		}
+		
+		adminService.decreaseBookQuantity(bookId);
+		
+	}
+	
+	@DeleteMapping("/secure/delete/book")
+	public void deleteBook(@RequestHeader(value = "Authorization") String token,
+							@RequestParam Long bookId) throws Exception {
+		
+		String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+		if (admin == null || !admin.equals("admin")) {
+			throw new Exception("Admin page only");
+		}
+		
+		adminService.deleteBook(bookId);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/luv2code/springbootlibrary/controller/MessagesController.java
@@ -0,0 +1,58 @@
+package com.luv2code.springbootlibrary.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.springbootlibrary.entity.Message;
+import com.luv2code.springbootlibrary.requestmodels.AdminQuestionRequest;
+import com.luv2code.springbootlibrary.service.MessagesService;
+import com.luv2code.springbootlibrary.utils.ExtractJWT;
+
+@CrossOrigin("https://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+	private MessagesService messagesService;
+
+	@Autowired
+	public MessagesController(MessagesService messagesService) {
+		this.messagesService = messagesService;
+	}
+	
+	
+	@PostMapping("/secure/add/message")
+	public void postMessage(@RequestHeader(value = "Authorization") String token,
+							@RequestBody Message messageRequest) {
+		String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+		messagesService.postMessage(messageRequest, userEmail);
+	}
+	
+	@PutMapping("/secure/admin/message")
+	public void putMessage(@RequestHeader(value = "Authorization") String token,
+				@RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception{
+		String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+		String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+		
+		if (admin == null || !admin.equals("admin")) {
+			throw new Exception("Admin page only");
+		}
+		messagesService.putMessage(adminQuestionRequest, userEmail);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
